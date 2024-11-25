0a4a5d21ef89e0e32d6aa759fc545dddfb4c3db8
ChoiDevv <oinkoink71@naver.com>
2024-11-26 05:14:27 +0900

    11727 - re

---
+++ remind/boj/11727.py
@@ -0,0 +1,15 @@
+# 2xn 타일링 2
+# boj.kr/11727
+
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0] * 1001
+
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3, n + 1):
+    dp[i] = 2 * dp[i - 2] + dp[i - 1]
+
+print(dp[n] % 10007)
